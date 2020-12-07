b686c3584efe8a01f92f59aea8bf8fea6075951f
Nicholas Leal <nleal05@safeway.com>
2020-12-07 14:31:20 -0800

    template add

--- index.php
+++ index.php
@@ -1,10 +1,229 @@
-<html>
-	<head>
-	<title> Cubie</title>
-	</head>
-	<body>
-		<?php
-			echo "<p> Cubie Jones!</p>";
-		?>
-	</body>
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>News Theme</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700" rel="stylesheet">
+    <!--    <link rel="stylesheet" type="text/css" href="css/animate.css" /> -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="brand-sec">
+                <img src="images/logo.png" alt="logo">
+            </div>
+            <div class="content">
+                <h5>Lorem Ipsum is simply dummy text of the  <br/>printing and typesetting industry. </h5>
+            </div>
+        </div>
+    </header>
+    <section class="banner-sec" style="background:url(./images/banner-img.jpg)no-repeat;">
+        <div class="cover"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <h1>Promote your product 
+                        with this template</h1>
+                    <h4>Best one page website template 
+                            for your product</h4>
+                    <div class="button-sec">
+                        <a href="inner.html" class="btn buy-btn">Buy Now</a>
+                        <a href="details.html" class="btn details-btn">Details</a>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="product-img">
+                        <img src="images/product-img.jpg">
+                        <div class="price">
+                            39<span>$</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="featured-sec">
+        <div class="container">
+            <div class="title-sec">
+                <span class="plane-icon"><i class="fas fa-paper-plane"></i></span>
+                <h2>Featured of the product</h2>
+                <h5>Some silent features of the product are below</h5>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="feature-item">
+                        <h3>Completely responsive</h3>
+                        <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</h6>
+                        <span class="certi"><i class="fas fa-certificate"></i></span>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="feature-item">
+                        <h3>Unlimited color schemes</h3>
+                        <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</h6>
+                        <span class="certi"><i class="fas fa-certificate"></i></span>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="feature-item">
+                        <h3>HTML5 and CSS3 power</h3>
+                        <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</h6>
+                        <span class="certi"><i class="fas fa-certificate"></i></span>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="feature-item">
+                        <h3>500+ Google Webfonts</h3>
+                        <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</h6>
+                        <span class="certi"><i class="fas fa-certificate"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="preview-sec" style="background:url(./images/preview-bg.jpg)no-repeat;">
+        <div class="layer"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Watch the preview</h2>
+                    <h4>A small video of your product</h4>
+                    <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum type and scrambled it to make a type specimen book. It has survived not only five</h6>
+                    <a href="inner.html" class="btn buy-btn">Buy Now</a>
+                </div>
+                <div class="col-md-6">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" width="640" height="640" src="https://www.youtube.com/embed/o1nCXzN6ihI" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="product-gallery">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <figure class="product">
+                        <img src="images/product-img.jpg" alt="product">
+                    </figure>
+                </div>
+                <div class="col-md-6">
+                    <div class="product-content">
+                        <h2>Watch the preview</h2>
+                        <h4>A small video of your product</h4>
+                        <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum type and scrambled it to make a type specimen book. It has survived not only five</h6>
+                        <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum type and scrambled it to make a type specimen book. It has survived not only five</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="row products-group">
+                <div class="col col-md-4 col-12">
+                    <figure>
+                        <img src="images/p-img.jpg" alt="p1">
+                    </figure>
+                </div>
+                <div class="col col-md-4 col-12">
+                    <figure>
+                        <img src="images/p-img.jpg" alt="p1">
+                    </figure>
+                </div>
+                <div class="col col-md-4 col-12">
+                    <figure>
+                        <img src="images/p-img.jpg" alt="p1">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="testimonal-sec">
+        <div class="container">
+            <div class="testimonal">
+                <div class="title-sec">
+                    <span class="heart"><i class="far fa-heart"></i></span>
+                    <h3>Client Testimonials</h3>
+                    <h5>see what product buyers say about it!</h5>
+                </div>
+                <div class="quote-sec">
+                    <ul>
+                        <li>
+                            <div class="quote">
+                                <figure>
+                                    <img src="images/quote.jpg" alt="client">
+                                </figure>
+                            </div>
+                            <div class="testimonal-text">
+                                <blockquote>Lorem Ipsum is simply dummy text of the printing and types etting industry. Lorem galley of type and scrambled it to make a type specimen book. has unknown printer took a galley of type and scrambled it to make a type specimen book. </blockquote>
+                                <h5><strong>Andrew Mathew</strong></h5>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="quote">
+                                <figure>
+                                    <img src="images/quote.jpg" alt="client">
+                                </figure>
+                            </div>
+                            <div class="testimonal-text">
+                                <blockquote>Lorem Ipsum is simply dummy text of the printing and types etting industry. Lorem galley of type and scrambled it to make a type specimen book. has been galley of type and scrambled it to make a type specimen book. when an unknown printer took a galley of type and scrambled it to make a type specimen book </blockquote>
+                                <h5><strong>Andrew Mathew</strong></h5>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="contact-sec">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-12">
+                    <div class="mail">
+                        <span><i class="far fa-envelope"></i></span><a href="mailto:Hello@mysite.com">Hello@mysite.com</a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-12">
+                    <div class="phone">
+                        <span><i class="fas fa-phone-square"></i></span>800 1230 4569
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="share">
+                        <ul>
+                            <li>
+                                <a href=""> <span class="fab fa-facebook"></span></a>
+                            </li>
+                            <li>
+                                <a href=""> <span class="fab fa-instagram"></span></a>
+                            </li>
+                            <li>
+                                <a href=""> <span class="fab fa-twitter"></span></a>
+                            </li>
+                            <li>
+                                <a href=""> <span class="fab fa-pinterest"></span></a>
+                            </li>
+                            <li>
+                                <a href=""> <span class="fab fa-google-plus-g"></span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <h5>(C) 2017. All Rights Reserved. <a href="#">Single Product Template</a></h5>
+        </div>
+    </footer>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js">
+    </script>
+    <script src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+</body>
+
+</html>
